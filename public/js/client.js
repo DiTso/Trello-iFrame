cf67aad6001d0e277c0a5d0e28e84204a07997c7
System <none>
2017-05-21 22:26:36 +0000

    Checkpoint

--- public/js/client.js
+++ public/js/client.js
@@ -1,76 +1,8 @@
 /* global TrelloPowerUp */
 
-// we can access Bluebird Promises as follows
 var Promise = TrelloPowerUp.Promise;
 
-/*
-
-Trello Data Access
-
-The following methods show all allowed fields, you only need to include those you want
-They all return promises that resolve to an object with the requested fields
-
-Get information about the current board
-t.board('id', 'name', 'url', 'shortLink', 'members')
-
-Get information about the current list (only available when a specific list is in context)
-So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
-t.list('id', 'name', 'cards')
-
-Get information about all open lists on the current board
-t.lists('id', 'name', 'cards')
-
-Get information about the current card (only available when a specific card is in context)
-So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
-t.card('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
-
-Get information about all open cards on the current board
-t.cards('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
-
-Get information about the current active Trello member
-t.member('id', 'fullName', 'username')
-
-*/
-
-/*
-
-Storing/Retrieving Your Own Data
-
-Your Power-Up is afforded 4096 chars of space per scope/visibility
-The following methods return Promises.
-
-Storing data follows the format: t.set('scope', 'visibility', 'key', 'value')
-With the scopes, you can only store data at the 'card' scope when a card is in scope
-So for example in the context of 'card-badges' or 'attachment-sections', but not 'board-badges' or 'show-settings'
-Also keep in mind storing at the 'organization' scope will only work if the active user is a member of the team
-
-Information that is private to the current user, such as tokens should be stored using 'private'
-
-t.set('organization', 'private', 'key', 'value');
-t.set('board', 'private', 'key', 'value');
-t.set('card', 'private', 'key', 'value');
-
-Information that should be available to all users of the Power-Up should be stored as 'shared'
-
-t.set('organization', 'shared', 'key', 'value');
-t.set('board', 'shared', 'key', 'value');
-t.set('card', 'shared', 'key', 'value');
-
-If you want to set multiple keys at once you can do that like so
-
-t.set('board', 'shared', { key: value, extra: extraValue });
-
-Reading back your data is as simple as
-
-t.get('organization', 'shared', 'key');
-
-Or want all in scope data at once?
-
-t.getAll();
-
-*/
-
-var SIGMA_ICON = 'https://cdn.glitch.com/380a7bed-fba7-4128-9418-b75f0d1d7492%2Fsigma2.svg?1495404692363';
+var SIGMA_ICON = 'https://cdn.glitch.com/380a7bed-fba7-4128-9418-b75f0d1d7492%2Fsigma_final.svg?1495405328591';
 
 var getBadges = function(t){
   return t.get('board', 'shared', 'costs')
@@ -87,18 +19,7 @@ var getBadges = function(t){
   });
 };
 
-var boardButtonCallback = function(t) {
-  return false;
-};
-
 var cardButtonCallback = function(t){
-  // Trello Power-Up Popups are actually pretty powerful
-  // Searching is a pretty common use case, so why reinvent the wheel
-  
-  // in the above case we let Trello do the searching client side
-  // but what if we don't have all the information up front?
-  // no worries, instead of giving Trello an array of `items` you can give it a function instead
-
   
   return t.get('board', 'shared', 'costs')
   .then(function(costs){
@@ -121,9 +42,6 @@ var cardButtonCallback = function(t){
               }
             }
           ];
-          // use options.search which is the search text entered so far
-          // and return a Promise that resolves to an array of items
-          // similar to the items you provided in the client side version above
         },
         search: {
           placeholder: 'Enter Cost',
@@ -136,18 +54,7 @@ var cardButtonCallback = function(t){
 
 };
 
-// We need to call initialize to get all of our capability handles set up and registered with Trello
 TrelloPowerUp.initialize({
-  // NOTE about asynchronous responses
-  // If you need to make an asynchronous request or action before you can reply to Trello
-  // you can return a Promise (bluebird promises are included at TrelloPowerUp.Promise)
-  // The Promise should resolve to the object type that is expected to be returned
-  'authorization-status': function(t, options){
-    // return a promise that resolves to the object with
-    // a property 'authorized' being true/false
-    // you can also return the object synchronously if you know the answer synchronously
-    return new TrelloPowerUp.Promise((resolve) => resolve({ authorized: true }));
-  },
   'board-buttons': function(t, options){
     
     return t.get('board', 'shared', 'costs')
@@ -161,7 +68,6 @@ TrelloPowerUp.initialize({
         // that callback function should probably open a popup, overlay, or boardBar
         icon: SIGMA_ICON,
         text: `Total Cost: ${totalCost.toFixed(2)}`,
-        callback: boardButtonCallback
       }];
     });
   },
@@ -193,17 +99,6 @@ TrelloPowerUp.initialize({
       
     });
   },
-  'show-authorization': function(t, options){
-    // return what to do when a user clicks the 'Authorize Account' link
-    // from the Power-Up gear icon which shows when 'authorization-status'
-    // returns { authorized: false }
-    // in this case we would open a popup
-    return t.popup({
-      title: 'My Auth Popup',
-      url: './authorize.html', // this page doesn't exist in this project but is just a normal page like settings.html
-      height: 140,
-    });
-  }
 });
 
 console.log('Loaded by: ' + document.referrer);
