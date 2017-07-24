25f4a95cb84c8e7de40221533fa9519e834539eb
System <none>
2017-07-24 17:39:48 +0000

    Checkpoint

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# Trello Costs Power-Up 🚀
+# Trello iFrame Power-Up 🚀
 
 Hey there 👋
 
-This is a Trello Power-Up which lets you add costs to cards and provides a sum of those costs as a board item.
+This is a Trello Power-Up which lets you Use Trello's board bar to nest an iFrame of your choice in your board.
 
 ![](https://cdn.glitch.com/380a7bed-fba7-4128-9418-b75f0d1d7492%2FScreen%20Shot%202017-05-21%20at%204.31.26%20PM.png?1495409557125)
 
-![](https://cdn.glitch.com/380a7bed-fba7-4128-9418-b75f0d1d7492%2FScreen%20Shot%202017-05-21%20at%204.31.50%20PM.png?1495409554873)
 
 To enable this Power-Up for your Trello boards, you'll need to go here:
 
@@ -14,7 +13,7 @@ To enable this Power-Up for your Trello boards, you'll need to go here:
 
 Select the Trello team you want to add the Power-Up to. Note: You need to be an admin of the Trello team to add custom Power-Ups to it.
 
-Now click the `Create new Power-Up` button. If this is your first time adding a Power-Up, you'll need to agree to a "Joint Developer Agreement" first. After you have done that, you just need to give your Power-Up a name, and paste this app's manifest url, `https://trello-costs.glitch.me/manifest.json`.
+Now click the `Create new Power-Up` button. If this is your first time adding a Power-Up, you'll need to agree to a "Joint Developer Agreement" first. After you have done that, you just need to give your Power-Up a name, and paste this app's manifest url, `https://trello-iframe.glitch.me/manifest.json`.
 
 Click `Save` and it's time to celebrate 🎉 🎊
 
--- public/js/client.js
+++ public/js/client.js
@@ -8,57 +8,68 @@ TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return t.get('board', 'shared', 'iframe')
     .then(function(iframe){
-    return [{
-      icon: './images/icon.svg',
-      text: 'Open iFrame',
-      callback: function(t) {
-          return t.popup({
-            title: 'Trello iFrames',
-            items: function(t, options) {
-              return [
-                {
-                  text: Number.isNaN(parseFloat(options.search))  ? (options.search ? `Set iFrame URL.` : `close iFrame`) : `Set iFrame height.`,
-                  callback: function(t) {
-                    if (options.search) {
-                      if (Number.isNaN(parseFloat(options.search))) {
-                        return t.set('board', 'shared', 'iframe', {
-                          url: options.search,
-                          height: iframe && iframe.height ? iframe.height: 500
-                        })
-                        .then(function(){
-                          return t.boardBar({
-                            url: options.search,
-                            height: iframe && iframe.height ? iframe.height : 500
-                          }).then(function(){t.closePopup});
-                        });
-                      } else {
-                        if (iframe.url) {
+      if (iframe) {
+        t.boardBar({
+          url: iframe.url ? iframe.url : '',
+          height: iframe.height ? iframe.height : 500
+        });
+      }
+      return [{
+        icon: './images/icon.svg',
+        text: 'Open iFrame',
+        callback: function(t) {
+            return t.popup({
+              title: 'Trello iFrames',
+              items: function(t, options) {
+                return [
+                  {
+                    text: Number.isNaN(parseFloat(options.search))  ? (options.search ? `Set iFrame.` : `Close iFrame.`) : `Set iFrame height.`,
+                    callback: function(t) {
+                      if (options.search) {
+                        if (Number.isNaN(parseFloat(options.search))) {
+                          var a = document.createElement('a');
+                          a.href = options.search;
+                          if (!a.host) {
+                            a.href = 'https://google.com/search?q='
+                          }
                           return t.set('board', 'shared', 'iframe', {
-                            url: iframe && iframe.url ? iframe.url : '',
-                            height: options.search
+                            url: options.search,
+                            height: iframe && iframe.height ? iframe.height: 500
                           })
                           .then(function(){
                             return t.boardBar({
-                              url: iframe && iframe.url ? iframe.url : '',
-                              height: options.search
+                              url: options.search,
+                              height: iframe && iframe.height ? iframe.height : 500
                             }).then(function(){t.closePopup});
                           });
+                        } else {
+                          if (iframe.url) {
+                            return t.set('board', 'shared', 'iframe', {
+                              url: iframe && iframe.url ? iframe.url : '',
+                              height: options.search
+                            })
+                            .then(function(){
+                              return t.boardBar({
+                                url: iframe && iframe.url ? iframe.url : '',
+                                height: options.search
+                              }).then(function(){t.closePopup});
+                            });
+                          }
                         }
+                      } else {
+                        t.closeBoardBar();
                       }
-                    } else {
-                      t.closeBoardBar();
+                      return t.closePopup();
                     }
-                    return t.closePopup();
                   }
-                }
-              ];
-            },
-            search: {
-              placeholder: 'Enter URL (or desired height)',
-            }
-          });
-        });
-      }
-    }];
+                ];
+              },
+              search: {
+                placeholder: 'Enter URL, search query, or desired height.',
+              }
+            });
+        }
+      }];
+    });
   },
 });
